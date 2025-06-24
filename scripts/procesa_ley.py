98a2555ffb0812ecb811140bb532a65ba50de6ba
engemac <63323233+engemac@users.noreply.github.com>
2025-06-24 15:03:34 -0600

    Update procesa_ley.py

--- scripts/procesa_ley.py
+++ scripts/procesa_ley.py
@@ -1,17 +1,8 @@
 # scripts/procesa_ley.py
 
 import os
-from docx import Document
 from PyPDF2 import PdfReader
 from limpia_texto import limpiar_texto
-
-def extraer_txt_desde_doc(path):
-    try:
-        doc = Document(path)
-        return "\n".join([p.text for p in doc.paragraphs])
-    except Exception as e:
-        print(f"[!] Error procesando DOC: {e}")
-        return ""
 
 def extraer_txt_desde_pdf(path):
     try:
@@ -31,13 +22,10 @@ def procesar_ley(nombre, siglas):
     carpeta = f"output/{siglas}"
     os.makedirs(carpeta, exist_ok=True)
 
-    doc_path = os.path.join(carpeta, f"{siglas}.doc")
     pdf_path = os.path.join(carpeta, f"{siglas}.pdf")
 
     texto = ""
-    if os.path.exists(doc_path):
-        texto = extraer_txt_desde_doc(doc_path)
-    elif os.path.exists(pdf_path):
+    if os.path.exists(pdf_path):
         texto = extraer_txt_desde_pdf(pdf_path)
     
     if texto:
